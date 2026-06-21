42195729e7e4971e7b0c20313c45d728527005e4
fajardo01 <ernestoafajardo2001@gmail.com>
2026-06-21 19:40:16 +0200

    added UMAPPlot

--- qsprpred/plotting/chemicalspace.py
+++ qsprpred/plotting/chemicalspace.py
@@ -38,3 +38,37 @@ class PCAPlot:
         fig.savefig(out_path, dpi=200, bbox_inches="tight")
         plt.close(fig)
         return X_pca
+
+
+class UMAPPlot:
+    """Class to generate UMAP plots for a dataset's descriptors."""
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def make(self, out_path="fingerprint_umap.jpg"):
+        df = self.dataset.getDescriptors()
+        numeric_df = df.select_dtypes(include=[np.number, bool]).astype(int)
+
+        reducer = umap.UMAP(n_components=2, random_state=42)
+        X_umap = reducer.fit_transform(numeric_df)
+
+        plot_df = pd.DataFrame(X_umap, columns=["UMAP1", "UMAP2"])
+        fig, ax = plt.subplots(figsize=(10, 8))
+        ax.scatter(
+            plot_df["UMAP1"],
+            plot_df["UMAP2"],
+            alpha=0.7,
+            edgecolors="DarkSlateGrey",
+            s=50,
+            c="royalblue",
+        )
+
+        ax.set_title("Fingerprint UMAP Visualization")
+        ax.set_xlabel("UMAP1")
+        ax.set_ylabel("UMAP2")
+        ax.grid(True, linestyle="--", alpha=0.3)
+
+        fig.savefig(out_path, dpi=200, bbox_inches="tight")
+        plt.close(fig)
+        return X_umap
